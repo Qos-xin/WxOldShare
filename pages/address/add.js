0b87bc8b88b7efefac17229985038775442a6005
Qos-xin <hui5217758521@163.com>
2018-06-02 00:53:59 +0800

    修改

--- pages/address/add.js
+++ pages/address/add.js
@@ -1,20 +1,31 @@
 // pages/address/add.js
-var util=require("../../utils/util.js");
+import { validate, validateRequired } from "../../utils/validate.js"
+const util = require("../../utils/util.js")
+let self = {}
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    submiting: false,
+    name:"",
+    phone:"",
+    address:""
   },
   onSubmit: function (event) {
-    var that = this;  
+    validateRequired(['name', 'phone', 'address'], self)
+    if ('' === self.data.form.$invalidMsg) {
+      console.log('invalid')
+    } else {
+      console.log('valid')
+    }
+    var that = this;
     var token = wx.getStorageSync("token");
     var formData = event.detail.value;
     console.log("hah" + JSON.stringify(formData))
-    util.http.post("/api/User/AddContect",formData)
-    .then(data=>{
+    util.http.post("/api/User/AddContect", formData)
+      .then(data => {
         wx.showToast({
           title: '添加成功',
           icon: "success"
@@ -22,13 +33,19 @@ Page({
         wx.navigateBack({
           delta: 1
         })
+      })
+  },
+  validate(e) {
+    this.setData({
+      [e.currentTarget.dataset.name]: e.detail.value
     })
+    validate(e, this)
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    self = this
   },
 
   /**
